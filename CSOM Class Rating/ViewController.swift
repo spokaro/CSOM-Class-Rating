d35a1c4d8ba9389bb0e85437a716393263522ae7
andres <spokaro21@gmail.com>
2019-12-12 17:01:49 -0500

    Initial Commit

---
+++ CSOM Class Rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSOM Class Rating
+//
+//  Created by Andrew Boucher on 12/12/19.
+//  Copyright © 2019 Andres de la Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
