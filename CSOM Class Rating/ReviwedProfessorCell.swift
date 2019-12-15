20d0208ddbca60512e74b87d086aea240da24781
andres <spokaro21@gmail.com>
2019-12-15 11:56:01 -0500

    hello

---
+++ CSOM Class Rating/ReviwedProfessorCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  CSOM Class Rating
+//
+//  Created by Andrew Boucher on 12/12/19.
+//  Copyright © 2019 Andres de la Cruz. All rights reserved.
+//
+
+
--- CSOM Class Rating/ViewController.swift
+++ CSOM Class Rating/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var concentration = Concentration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toConcentration" {
+            let destination = segue.destination as! ConcentrationDetailViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            destination.currentConcentration = concentration.concentrationsArray[index]
+        }else {
+            if let selectedPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedPath, animated: false)
+            }
+        }
+    }
 }
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return concentration.concentrationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = concentration.concentrationsArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
